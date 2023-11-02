e0becb5ab25c03547cadd9894c863725748207b8
30Piraten <kioyarautenberg@gmail.com>
2023-11-02 21:41:21 +0200

    update sys_argv file

--- command_line_3/case_study/sys_argv.py
+++ command_line_3/case_study/sys_argv.py
@@ -2,35 +2,32 @@ import sys
 
 # script that prints "command line" to the console
 # only when run from the command line
-
-# define a function for this
+# -- added client "--prompt"
 
 
 def cli(text, client):
     send_message = f"{text} {client}"
-    print(send_message)
+    return send_message
 
 
 if __name__ == "__main__":
     text = "Command Line"
     client = "Administrator"
-    # Here, user is None to avoid name || prompt error
-    # when passed to cli function.
-    # can also equate user to be "Administrator
-    # as default || set as None"
-    user = None  # Administrator
+    user = None  # Default value
 
     if "--help" in sys.argv:
         help = f"Usage: {sys.argv[0]} --prompt <ROLE>"
         print(help)
         sys.exit(0)
 
-    # Here I have added a logic for users
-    # to be able to change the client's name
     if "--prompt" in sys.argv:
-        # get the position after prompt flag
+        # check if there enough arguments after "--prompt"
         index_of_prompt = sys.argv.index("--prompt") + 1
         if index_of_prompt < len(sys.argv):
             user = sys.argv[index_of_prompt]
+        else:
+            print("Error: Missing user role after --prompt")
+            sys.exit(1)
 
-    cli(text, user)
+    result = cli(text, user)
+    print(result)
